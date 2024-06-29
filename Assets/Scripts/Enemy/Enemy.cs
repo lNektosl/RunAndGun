57bc75fe830233f25e0774f6af5ca0d60de0127a
lNektosl <105857065+lNektosl@users.noreply.github.com>
2024-06-29 20:37:26 +0300

    Added active/deactive enemy
    MoveToCenter + active/deactive state

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,7 +8,8 @@ public class Enemy : MonoBehaviour, IDamagable {
 
     private Transform player;
 
-    private int hp = 5;
+    private bool isActive = false;
+    private int hp = 6;
     private Collider2D collider;
 
     private Rigidbody2D rigidbody;
@@ -25,59 +26,65 @@ public class Enemy : MonoBehaviour, IDamagable {
     float dirCouldown;
 
     private bool isDead = false;
-    private void Awake() {
+    private void Awake () {
         rigidbody = GetComponent<Rigidbody2D>();
         player = FindObjectOfType<Player>().transform;
         collider = GetComponent<Collider2D>();
+
+
     }
-    private void Start() {
+    private void Start () {
         rigidbody.freezeRotation = true;
-        targetDir = transform.up;
+        targetDir = new Vector2(0, 0);
         dirCouldown = Random.Range(1f, 5f);
+        Physics2D.IgnoreLayerCollision(6, 8, true);
+
     }
 
-    private void FixedUpdate() {
-        if (!isDead) {
+    private void FixedUpdate () {
+        if (!isDead || isActive) {
             UpdateTargetDir();
             IsAware();
             HandleRotation();
             HandleMovment();
         }
+        if (!isActive) {
+            MoveToCenter();
+        }
     }
 
-    private void HandleRotation() {
-                if(targetDir!= Vector2.zero) {
+    private void HandleRotation () {
+        if (targetDir != Vector2.zero) {
             Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDir);
             Quaternion rotate = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             rigidbody.SetRotation(rotate);
         }
     }
 
-    private void UpdateTargetDir() {
+    private void UpdateTargetDir () {
         toTargetVector = player.position - transform.position;
     }
 
-    private void IsAware() {
+    private void IsAware () {
         if (toTargetVector.magnitude <= awarensesRange) {
             isAwareOfThePlayer = true;
-        }
-        else {
+        } else {
             isAwareOfThePlayer = false;
         }
     }
 
 
-    private void HandleMovment() {
+    private void HandleMovment () {
         HandleRandomDirection();
         HandlePlayerTargeting();
         isMoving = rigidbody.velocity != Vector2.zero;
     }
 
-    private void HandleRandomDirection() {
-        
+    private void HandleRandomDirection () {
+
         dirCouldown -= Time.deltaTime;
 
-        if (dirCouldown <= 0) {
+        if (dirCouldown <= 0 && isActive) {
             GetRandomDir();
             dirCouldown = Random.Range(1f, 5f);
         }
@@ -86,57 +93,68 @@ public class Enemy : MonoBehaviour, IDamagable {
 
     }
 
-    private void GetRandomDir(){
+    private void GetRandomDir () {
         float angleChange = Random.Range(-90f, 90f);
         Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
         targetDir = rotation * targetDir;
     }
 
 
-    private void HandlePlayerTargeting() {
+    private void HandlePlayerTargeting () {
         if (isAwareOfThePlayer) {
             targetDir = toTargetVector.normalized;
             rigidbody.velocity = targetDir * moveSpeed;
         }
     }
 
-    public void TakeDamage(int damage) {
+    public void TakeDamage (int damage) {
         hp -= damage;
         if (hp <= 0) {
             Die();
         }
     }
 
-    public bool IsMoving() {
+    public bool IsMoving () {
         return isMoving;
     }
 
-    public bool IsDead() {
+    public bool IsDead () {
         return isDead;
     }
 
-    private void Die() {
+    private void Die () {
         rigidbody.velocity = Vector2.zero;
         isMoving = false;
         isDead = true;
         collider.enabled = false;
         Destroy(gameObject, 3f);
     }
-    public void OnCollisionStay2D(Collision2D collision) {
+    public void OnCollisionStay2D (Collision2D collision) {
         IDamagable damagable = collision.gameObject.GetComponent<IDamagable>();
         if (damagable != null) {
             damagable.TakeDamage(damege);
         }
     }
 
-    public void OnCollisionEnter2D(Collision2D collision){
+    public void OnCollisionEnter2D (Collision2D collision) {
         GameObject gameObject = collision.gameObject;
         int layer = gameObject.layer;
-         
-        if (LayerMask.LayerToName(layer) == "Boundry"){
+
+        if (LayerMask.LayerToName(layer) == "Boundary" && isActive) {
             targetDir = -targetDir;
             rigidbody.velocity = targetDir * moveSpeed;
         }
     }
 
+    public void OnTriggerExit2D (Collider2D collision) {
+        isActive = true;
+        Physics2D.IgnoreLayerCollision(6, 8, false);
+    }
+
+
+    private void MoveToCenter () {
+        Vector2 screenCenter = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width / 2, Screen.height / 2));
+        targetDir = (screenCenter - (Vector2)transform.position).normalized;
+        rigidbody.velocity = targetDir * moveSpeed;
+    }
 }
